0a592443771e54dfcae412eb692429099e16e879
mayralihue131 <mayralihue131@gmail.com>
2025-08-17 18:46:09 -0300

    Create ej4.c

---
+++ ej4.c
@@ -0,0 +1,12 @@
+#include <sdtio.h>
+int main(){
+  int edad;
+  printf ("ingrese su edad\n");
+  scanf ("%d", &edad);
+  if(edad => 18){
+    printf ("usted es mayor de edad");
+  }else{
+    printf ("no es mayor de edad");
+  }
+  
+}return 0;
